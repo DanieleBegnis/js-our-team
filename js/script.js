c657a60f906434705f8911b5d135ca9b0670ea0a
DanieleBegnis <daniele.begnis@gmail.com>
2024-03-20 16:16:28 +0100

    feat: add images

--- js/script.js
+++ js/script.js
@@ -13,32 +13,32 @@ const colleagues = [
     {
         name: 'Wayne Barnett',
         role: 'Founder & CEO',
-        Image: 'wayne-barnett-founder-ceo.jpg'
+        image: 'wayne-barnett-founder-ceo.jpg'
     },
     {
         name: 'Angela Caroll',
         role: 'Chief editor',
-        Image: 'angela-caroll-chief-editor.jpg'
+        image: 'angela-caroll-chief-editor.jpg'
     },
     {
         name: 'Walter Gordon',
         role: 'Office manager',
-        Image: 'walter-gordon-office-manager.jpg'
+        image: 'walter-gordon-office-manager.jpg'
     },
     {
         name: 'Angela Lopez',
         role: 'Social media manager',
-        Image: 'angela-lopez-social-media-manager.jpg'
+        image: 'angela-lopez-social-media-manager.jpg'
     },
     {
         name: 'Scott Estrada',
         role: 'Developer',
-        Image: 'scott-estrada-developer.jpg'
+        image: 'scott-estrada-developer.jpg'
     },
     {
         name: 'Barbara Ramos',
         role: 'Graphic designer',
-        Image: 'barbara-ramos-graphic-designer.jpg'
+        image: 'barbara-ramos-graphic-designer.jpg'
     }
 ]
 console.log(colleagues)
@@ -57,8 +57,8 @@ for (let i = 0; i < colleagues.length; i++) {
     const colleagueList = `
         <li> 
             <h4>${colleague.name}</h4> 
-            <h5>ROLE</h5>
-            <p>IMAGE <img src="img/" alt=""></p>
+            <h5>${colleague.role}</h5>
+            <p><img src="img/${colleague.image}" alt=""></p>
         </li>
     `
     mainList.innerHTML += colleagueList;
