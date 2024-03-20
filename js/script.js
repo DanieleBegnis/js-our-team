a2c7de3cc12696d84bd93eaab665102a73815748
DanieleBegnis <daniele.begnis@gmail.com>
2024-03-20 15:44:02 +0100

    feat: add object with colleagues

--- js/script.js
+++ js/script.js
@@ -5,3 +5,39 @@
 //MILESTONE 1:
 //Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
 //MILESTONE 2:
+//Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+
+//creo array per rappresentare i membri del team
+const colleagues = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        Image: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name: 'Angela Caroll',
+        role: 'Chief editor',
+        Image: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        name: 'Walter Gordon',
+        role: 'Office manager',
+        Image: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        name: 'Angela Lopez',
+        role: 'Social media manager',
+        Image: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        Image: 'scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic designer',
+        Image: 'barbara-ramos-graphic-designer.jpg'
+    }
+]
